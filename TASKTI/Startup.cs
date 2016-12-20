5b986f74b52f9700664f0326c1702f492b572711
ernandarn <ernandarn@gmail.com>
2016-12-20 14:53:50 +0700

    Add project files.

---
+++ TASKTI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TASKTI.Startup))]
+namespace TASKTI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
